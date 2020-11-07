3dc25b5c0fbe48e816d5a23280e2d5c44dfcb76c
ymartinguzman <yamiramartinguzman@gmail.com>
2020-11-07 20:18:50 +0100

    add jsx with map and filter price

--- Módulo-3/Lección-4/my-react-project-lesson-4/src/components/ItemList.js
+++ Módulo-3/Lección-4/my-react-project-lesson-4/src/components/ItemList.js
@@ -23,50 +23,36 @@ const arrayOfItems = [
     category: 'Bebida',
     price: 5,
   },
+  {
+    name: 'Bocadillo de chorizo',
+    quantity: 4,
+    category: 'Almuerzo',
+    price: 8,
+    description: 'Bocadillo premium de chorizo Pamplona',
+  },
 ];
+
+let emptyArray = [];
+
+let priceItems = [];
+priceItems = arrayOfItems.filter((itemPrice) => itemPrice.price < 10); //filter es como if
+
+emptyArray = priceItems.map((itemInfo, i) => (
+  //map es para recorrer
+  <li key={i}>
+    <Item
+      name={itemInfo.name}
+      description={itemInfo.description}
+      quantity={itemInfo.quantity}
+      category={itemInfo.category}
+      price={itemInfo.price}
+    />
+  </li>
+));
 
 class ItemList extends React.Component {
   render() {
-    return (
-      <ul className="item-list">
-        <li>
-          <Item
-            name="Cereales con chocolate"
-            description="Cereales rellenos de chocolate"
-            quantity={2}
-            category="Cereales"
-            price={5}
-          />
-        </li>
-        <li>
-          <Item
-            name="Hamburguesa con queso"
-            description="Hamburguesa rica y saludable"
-            quantity={1}
-            category="Fast-food"
-            price={15}
-          />
-        </li>
-        <li>
-          <Item
-            name="Agua mineral"
-            description="Agua de un charco del Himalaya"
-            quantity={2}
-            category="Bebida"
-            price={5}
-          />
-        </li>
-        <li>
-          <Item
-            name="Bocadillo de chorizo"
-            description="Bocadillo premium de chorizo Pamplona"
-            quantity={4}
-            category="Almuerzo"
-            price={8}
-          />
-        </li>
-      </ul>
-    );
+    return <ul className="item-list">{emptyArray}</ul>;
   }
 }
 
