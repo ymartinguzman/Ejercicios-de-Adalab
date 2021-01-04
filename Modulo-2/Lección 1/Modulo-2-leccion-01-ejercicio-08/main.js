a3b45a80636a6cd78ad31d778de32bb1252016ab
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-04 21:15:36 +0100

    add classList to exercise 8

--- Modulo-2/Lección 1/Modulo-2-leccion-01-ejercicio-08/main.js
+++ Modulo-2/Lección 1/Modulo-2-leccion-01-ejercicio-08/main.js
@@ -2,10 +2,13 @@
 
 const sectionA = document.querySelector('.section-a');
 const sectionB = document.querySelector('.section-b');
+const button1 = document.querySelector('.btn1');
+const button2 = document.querySelector('.btn2');
 
 //add elements
 sectionA.classList.add('hidden');
 sectionB.classList.add('change-color');
+button1.classList.add('change-btn');
 
 //remove elements
 sectionA.classList.remove('hidden');
