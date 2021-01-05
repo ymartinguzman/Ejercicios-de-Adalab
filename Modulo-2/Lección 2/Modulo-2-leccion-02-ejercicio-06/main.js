2bfcc01778feb7b913be45b7e0bb6534180ea661
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-05 11:56:48 +0100

    upload excercise

--- Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-06/main.js
+++ Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-06/main.js
@@ -1,10 +1,5 @@
-"use strict";
+'use strict';
 
-const sayHello = document.querySelector("h1");
-const name = document.querySelector(".name");
-
-name.innerHTML = "Lola";
-
-// sayHello.innerHTML = sayHello.innerHTML;
-
-// console.log( ${sayHello + name + "Lola"});
+const firstName = 'Lola';
+const sayHello = document.querySelector('h1');
+sayHello.innerHTML = `Hola ${firstName}, encantada de conocerte`;
