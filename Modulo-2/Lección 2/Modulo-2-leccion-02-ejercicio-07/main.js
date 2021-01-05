379eea853340544a99abe5094e04b6ff60b68804
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-05 12:12:30 +0100

    Module-02-lesson-02-exercise-07

--- Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-07/main.js
+++ Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-07/main.js
@@ -2,5 +2,5 @@
 
 const firstName = 'Lola';
 const sayHello = document.querySelector('h1');
-//sayHello.innerHTML = `Hola ${firstName}, encantada de conocerte`;
-sayHello.innerHTML = 'Hola ' + firstName + ', encantada de conocerte';
+sayHello.innerHTML = `Hola ${firstName}, encantada de conocerte`;
+//sayHello.innerHTML = 'Hola ' + firstName + ', encantada de conocerte';
