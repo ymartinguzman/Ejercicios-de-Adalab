b67d80ada38c06a99130cdacd51f377d4d1f3f22
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-05 12:40:40 +0100

    upload exercise 8

--- Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-08/main.js
+++ Modulo-2/Lección 2/Modulo-2-leccion-02-ejercicio-08/main.js
@@ -1,16 +1,14 @@
 const firstDogImage =
-  "https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg";
-const firstDogName = "Dina";
+  'https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg';
+const firstDogName = 'Dina';
 
 const secondDogImage =
-  "https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg";
-const secondDogName = "Luna";
+  'https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg';
+const secondDogName = 'Luna';
 
 const thirdDogImage =
-  "https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg";
-const thirdDogName = "Lana";
+  'https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg';
+const thirdDogName = 'Lana';
 
-const list = document.querySelector("ul");
-list.innerHTML = "${firstDogImage + secondDogImage + thirdDogImage}";
-
-// console.log();
+const dogList = document.querySelector('ul');
+dogList.innerHTML = `<li>${firstDogName} <img src="${firstDogImage}"/></li> <li>${secondDogName} <img src="${secondDogImage}"/></li> <li>${thirdDogName} <img src="${thirdDogImage}"/></li>`;
