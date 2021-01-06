e959384880a5ebfd3ec8f9d16671244a43c335c0
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-06 12:54:16 +0100

    module-02-lesson-03-exercise-03

---
+++ Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-03/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const age = 31;
+
+if (age < 32) {
+  console.log('el número es menor que el que tienes que adivinar');
+} else if (age > 32) {
+  console.log('el número es mayor que el que tienes que adivinar');
+} else {
+  console.log('lo has adivinado');
+}
