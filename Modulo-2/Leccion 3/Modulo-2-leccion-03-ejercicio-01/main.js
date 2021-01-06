62e8331a1cd814703a13e5594bdbd86b875358ef
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-06 12:26:17 +0100

    add condition

--- Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-01/main.js
+++ Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-01/main.js
@@ -1,12 +1,17 @@
 'use strict';
 
-// avatar por defecto
 const DEFAULT_AVATAR = 'http://placehold.it/300x300';
-// avatar que eligió el usuario al registrarse
 let userAvatar = 'http://www.fillmurray.com/300/300';
+//let userAvatar = '';
 
 const imageAvatar = document.querySelector('.user__avatar');
-//imageAvatar.src = userAvatar;
-imageAvatar.src = userAvatar || DEFAULT_AVATAR;
+//imageAvatar.src = userAvatar; //para mostrar la imagen
+//imageAvatar.src = userAvatar || DEFAULT_AVATAR;
 
 console.log(imageAvatar);
+
+if (userAvatar !== '') {
+  imageAvatar.src = userAvatar;
+} else {
+  imageAvatar.src = DEFAULT_AVATAR;
+}
