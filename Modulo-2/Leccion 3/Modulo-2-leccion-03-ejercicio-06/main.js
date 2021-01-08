3d62ae93c6c1bf4c573ba1154414a1a025ac24f8
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-08 18:06:18 +0100

    module-02-lesson-03-exercise-06

---
+++ Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-06/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// let avocados;
+// const avocadoPrice = 1.5;
+// const money = 33;
+
+// if (money >= avocadoPrice) {
+//   avocados = money / avocadoPrice;
+// } else {
+//   avocados = 0;
+// }
+
+//Reescribir código con Op ternario
+
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+
+avocados = money >= avocadoPrice ? money / avocadoPrice : (avocados = 0);
