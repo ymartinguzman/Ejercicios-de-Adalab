5e75ce13feb327bd238203685a2010ff42f3e86a
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-08 18:17:32 +0100

    module-02-lesson-03-exercise-07

---
+++ Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-07/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let ingredient = 'Merluza';
+
+switch (ingredient) {
+  case 'Merluza':
+    console.log('Has elegido merluzita en salsa verde');
+    break;
+  case 'Espinacas':
+    console.log('Has elegido espinacas rehogadas');
+    break;
+  case 'Pollo':
+    console.log('Has elegido pollo');
+    break;
+  default:
+    console.log('Nada en la nevera');
+}
