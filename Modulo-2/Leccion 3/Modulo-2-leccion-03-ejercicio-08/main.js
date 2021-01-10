3dd14c15e0bb9131132d0e0e14ba83dbe31af1c4
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-10 11:51:07 +0100

    add input value to year

--- Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-08/main.js
+++ Modulo-2/Leccion 3/Modulo-2-leccion-03-ejercicio-08/main.js
@@ -3,18 +3,21 @@
 const input = document.querySelector('.input');
 const result = document.querySelector('.result');
 
-const year = input.value;
+const year = input;
 const leapYear = 4;
 
 function years() {
+  debugger;
   if (year % 4 === 0) {
-    result.innerHTML = `Este año ${year} es bisiesto`; //${year + leapYear}
+    result.innerHTML = `Este año ${year} es bisiesto. El próximo año bisiesto será ${
+      year + leapYear
+    }`;
   } else if (year % 4 === 1) {
-    result.innerHTML = `${year + (leapYear - 3)}`;
+    result.innerHTML = `${year + 3}`;
   } else if (year % 4 === 2) {
-    result.innerHTML = `${year + (leapYear - 2)}`;
+    result.innerHTML = `${year + 2}`;
   } else if (year % 4 === 3) {
-    result.innerHTML = `${year + (leapYear - 1)}`;
+    result.innerHTML = `${year + 1}`;
   }
 }
 
