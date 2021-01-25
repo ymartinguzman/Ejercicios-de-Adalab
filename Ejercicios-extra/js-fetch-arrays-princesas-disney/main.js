20bca57c8f9708b84116b8ad7047f6e4cfc0e3b8
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-25 14:06:25 +0100

    call api throught fetch

---
+++ Ejercicios-extra/js-fetch-arrays-princesas-disney/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const princessList = document.querySelector('.list');
+
+let princess = [];
+function apiData(arr) {
+  fetch(
+    'https://beta.adalab.es/ejercicios-extra/js-fetch-arrays-princesas-disney/data/users.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      princess = data;
+      console.log(princess);
+    });
+  paintInfo(princess);
+}
+
+function paintInfo() {
+  for (let i = 0; i < princess.length; i++) {
+    console.log(princess[i].name);
+  }
+}
+
+apiData(princess);
